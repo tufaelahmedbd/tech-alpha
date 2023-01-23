fccb8795b2fc085366e0b926b4fa43e0f59fc363
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-01-23 23:35:28 +0600

    working on cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,20 @@
 const Cart = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="cart-section container mx-auto py-10">
+        <h2 className="section-title uppercase text-2xl font-bold space-font  text-center mb-10">
+          Your cart
+        </h2>
+      </div>
+      <div className="cart-container">
+        <div className="col-product">Product</div>
+        <div className="col-unit-price">Unit Price</div>
+        <div className="col-quantity">Quantity</div>
+        <div className="col-total-price">Total Price</div>
+      </div>
+      <div className="cart-lower"></div>
+    </div>
+  );
 };
 
 export default Cart;
